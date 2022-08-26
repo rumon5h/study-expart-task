933efda4b3f28798aa87886334cd77f3674dff29
rumon5 <mdrumonkhan48519@gmail.com>
2022-08-26 22:36:16 +0600

    Login UI done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import LogIn from './components/LogIn/LogIn';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/log-in' element={<LogIn/>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
 
 const LogIn = () => {
-    return (
-        <div>
+  return (
+    <div className="bg-white p-5 flex justify-center items-center h-[90vh] m-5">
+     <div className=" border-orange-500 shadow-orange-300 rounded-md border-2 shadow-lg p-10">
+     <form
+        className=""
+        action=""
+      >
+        <h1 className="text-2xl text-center mb-5">Please LogIn!</h1>
+        <input
+          className="mt-3 border-2 py-3 px-3 w-full min-w-[350px] block rounded-md outline-orange-500"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter your email"
+        />
+        <input
+          className="mt-3 border-2 py-3 px-3 w-full min-w-[350px] block rounded-md outline-orange-500"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter your password"
+        />
+        <input
+          className="mt-3 border-2 py-3 cursor-pointer rounded-md uppercase px-10 block w-full  text-orange-500 bg-orange-100  shadow hover:bg-orange-200"
+          type="submit"
+          value="Log In "
+        />
+       
+      </form>
+      <div className="grid grid-cols-3 my-3 w-full">
+          <span className=" border-t-4 border-orange-400 rounded-sm mt-2 w-full text-center">
             
+          </span>{" "}
+          <span className="text-gray-600 text-center">OR</span>{" "}
+          <span className=" border-t-4 border-orange-400 rounded-sm mt-2 w-full text-center">
+            
+          </span>
+
         </div>
-    );
+          <button className="mt-3 border-2 py-3 cursor-pointer rounded-md uppercase px-10 block w-full  text-orange-500 bg-orange-100  shadow hover:bg-orange-200"> Continue with google</button>
+     </div>
+    </div>
+  );
 };
 
 export default LogIn;
